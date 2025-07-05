68be0159cc833f68f163adc1163724bba0d96e42
somene112 <ndhphuc2005@gmail.com>
2025-07-06 00:35:32 +0700

    fix: increase wait time

--- run_denoise_app.bat
+++ run_denoise_app.bat
@@ -1,8 +1,6 @@
 @echo off
 echo 🔁 Starting Flask App...
 start cmd /k "cd /d D:\Nguyen_Duc_Hoang_Phuc\Lab\denoise_homework - submit && call conda activate anaconda && python app.py"
-
-timeout /t 6
-
+timeout /t 20
 echo 🌐 Launching Ngrok tunnel...
 start cmd /k "cd /d C:\Users\ndhph\Downloads\ngrok-v3-stable-windows-amd64 && ngrok http 5000"
